92fd54341f0d71c192ee28e31a107b5662c22c36
Dan Carley <dan.carley@gmail.com>
2014-10-13 21:13:47 +0100

    ginkgo for config_items

--- config_items_test.go
+++ config_items_test.go
@@ -1,48 +1,52 @@
-package main
+package main_test
 
 import (
-	"reflect"
-	"testing"
+	. "github.com/alphagov/cloudflare-configure"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-func TestUnionOfTwoConfigSets(t *testing.T) {
-	union := UnionConfigItems(
-		ConfigItems{
-			"always_online": "off",
-			"browser_check": "off",
-		},
-		ConfigItems{
-			"always_online":     "on",
-			"browser_cache_ttl": 14400,
+var _ = Describe("ConfigItems", func() {
+	Describe("Union", func() {
+		It("two ConfigItems", func() {
+			Expect(UnionConfigItems(
+				ConfigItems{
+					"always_online": "off",
+					"browser_check": "off",
+				},
+				ConfigItems{
+					"always_online":     "on",
+					"browser_cache_ttl": 14400,
+				},
+			)).To(Equal(
+				ConfigItems{
+					"always_online":     "on",
+					"browser_check":     "off",
+					"browser_cache_ttl": 14400,
+				},
+			))
 		})
+	})
 
-	expectedUnion := ConfigItems{
-		"always_online":     "on",
-		"browser_check":     "off",
-		"browser_cache_ttl": 14400,
-	}
-	if !reflect.DeepEqual(union, expectedUnion) {
-		t.Fatal("Expected to only receive the config items that are different", union)
-	}
-}
-
-func TestDifferenceOfConfigSets(t *testing.T) {
-	difference := DifferenceConfigItems(
-		ConfigItems{
-			"always_online": "off",
-			"browser_check": "off",
-		},
-		ConfigItems{
-			"always_online":     "on",
-			"browser_check":     "off",
-			"browser_cache_ttl": 14400,
+	Describe("Difference", func() {
+		It("two ConfigItems", func() {
+			Expect(DifferenceConfigItems(
+				ConfigItems{
+					"always_online": "off",
+					"browser_check": "off",
+				},
+				ConfigItems{
+					"always_online":     "on",
+					"browser_check":     "off",
+					"browser_cache_ttl": 14400,
+				},
+			)).To(Equal(
+				ConfigItems{
+					"always_online":     "on",
+					"browser_cache_ttl": 14400,
+				},
+			))
 		})
-
-	expectedDifference := ConfigItems{
-		"always_online":     "on",
-		"browser_cache_ttl": 14400,
-	}
-	if !reflect.DeepEqual(difference, expectedDifference) {
-		t.Fatal("Didn't receive the difference we were expecting", difference)
-	}
-}
+	})
+})
