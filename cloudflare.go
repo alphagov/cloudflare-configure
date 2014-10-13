029c462c5007382e0239e7dc97a49df9c55ffb37
Dan Carley <dan.carley@gmail.com>
2014-10-13 14:15:01 +0100

    auth email/key

--- cloudflare.go
+++ cloudflare.go
@@ -8,12 +8,23 @@ import (
 )
 
 type CloudFlareQuery struct {
-	RootURL string
+	RootURL   string
+	AuthEmail string
+	AuthKey   string
 }
 
 func (q *CloudFlareQuery) NewRequest(method, path string) (*http.Request, error) {
 	url := fmt.Sprintf("%s%s", q.RootURL, path)
-	return http.NewRequest(method, url, nil)
+
+	request, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Set("X-Auth-Email", q.AuthEmail)
+	request.Header.Set("X-Auth-Key", q.AuthKey)
+
+	return request, nil
 }
 
 type CloudFlare struct {
