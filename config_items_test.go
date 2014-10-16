382e6fe1bc3e59b59e80d5f87a40886dd9e5044b
Dan Carley <dan.carley@gmail.com>
2014-10-16 09:09:23 +0100

    Move cf.Compare() to CompareConfigItems()

--- config_items_test.go
+++ config_items_test.go
@@ -8,6 +8,78 @@ import (
 )
 
 var _ = Describe("ConfigItems", func() {
+	Describe("Compare()", func() {
+		settingValAlwaysOnline := "on"
+		settingValBrowserCache := 123
+
+		It("should return nothing when local and remote are identical", func() {
+			config, err := CompareConfigItems(
+				ConfigItems{
+					"always_online":     settingValAlwaysOnline,
+					"browser_cache_ttl": settingValBrowserCache,
+				},
+				ConfigItems{
+					"always_online":     settingValAlwaysOnline,
+					"browser_cache_ttl": settingValBrowserCache,
+				},
+			)
+
+			Expect(config).To(Equal(ConfigItems{}))
+			Expect(err).To(BeNil())
+		})
+
+		It("should return all items in local when remote is empty", func() {
+			config, err := CompareConfigItems(
+				ConfigItems{},
+				ConfigItems{
+					"always_online":     settingValAlwaysOnline,
+					"browser_cache_ttl": settingValBrowserCache,
+				},
+			)
+
+			Expect(config).To(Equal(ConfigItems{
+				"always_online":     settingValAlwaysOnline,
+				"browser_cache_ttl": settingValBrowserCache,
+			}))
+			Expect(err).To(BeNil())
+		})
+
+		It("should return one item in local overwriting always_online", func() {
+			config, err := CompareConfigItems(
+				ConfigItems{
+					"always_online":     "off",
+					"browser_cache_ttl": settingValBrowserCache,
+				},
+				ConfigItems{
+					"always_online":     settingValAlwaysOnline,
+					"browser_cache_ttl": settingValBrowserCache,
+				},
+			)
+
+			Expect(config).To(Equal(ConfigItems{
+				"always_online": settingValAlwaysOnline,
+			}))
+			Expect(err).To(BeNil())
+		})
+
+		It("should return a public error when item is missing in local", func() {
+			config, err := CompareConfigItems(
+				ConfigItems{
+					"always_online":     settingValAlwaysOnline,
+					"browser_cache_ttl": settingValBrowserCache,
+				},
+				ConfigItems{
+					"browser_cache_ttl": settingValBrowserCache,
+				},
+			)
+
+			Expect(config).To(BeNil())
+			Expect(err).ToNot(BeNil())
+			Expect(err).To(MatchError(
+				ConfigMismatch{Missing: ConfigItems{"always_online": settingValAlwaysOnline}}))
+		})
+	})
+
 	Describe("Union", func() {
 		It("merges two ConfigItems objects overwriting values of the latter with the former", func() {
 			Expect(UnionConfigItems(
